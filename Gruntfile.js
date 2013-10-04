c29b6a65237d6b9e7b3e192f0c8cd735defe6be2
maslen <tom_maslen@hotmail.com>
2013-10-04 11:00:48 +0100

    Creating repo

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+      connect: {
+        server: {
+          options: {
+            port: 9001,
+            base: '.',
+            keepalive: true
+          }
+        }
+      }
+    });
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('server', ['connect']);
+};
